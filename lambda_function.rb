16c22a2b1485426d7dddce63e616f919a87ea37b
javierf <javier.flores@loanpro.io>
2025-02-16 18:55:54 -0300

    deployment to render

---
+++ lambda_function.rb
@@ -0,0 +1,7 @@
+require 'bundler/setup'
+require 'lamby'
+require_relative 'config/environment'  # Adjust the path as necessary to load your Rails app
+
+def handler(event:, context:)
+  Lamby.handler(event, context)
+end
